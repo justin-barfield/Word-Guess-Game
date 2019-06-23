8b2bf0a8174667be5850073c31f183cfa84c5d44
barfiejus <justin.kyle.barfield@gmail.com>
2019-06-22 20:54:29 -0400

    Generating word and displaying to page. For loop set to run though letters in the word and announce whif it is correct. Issues with getting letters to append without an empty div between. Issues having score run properly alongside the checker.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,117 @@
 var log = console.log;
 
 var words = [
-    {q: 'help', a: 'help'},
-    {q: 'me', a: 'me'},
-    {q: 'with', a: 'with'},
-    {q: 'this', a: 'this'},
+    ['h', 'e', 'l', 'p'],
+    ['m', 'e'],
+    ['s', 'o', 'l', 'v', 'e'],
+    ['e','v','e','r','y','t','h','i','n','g'],
 ];
 
-var test = ["help"];
+var test = ["help", 'test', 'test'];
 
 //generate a random word from array
-var generateRandomWord = function(){
-    var randomNumber = Math.floor(Math.random() * words.length);
-    return words[randomNumber].q;
+var generateRandomNumber = Math.floor(Math.random() * words.length);
+
+var generateRandomWord = words[generateRandomNumber];
+// log(generateRandomWord);
+
+
+var randomWord = generateRandomWord.join(' ');
+
+// write word and dashes on screen
+// var writeWord = function(){
+//     document.getElementById('generated-word').innerHTML = randomWord;
+// };
+// writeWord();
+
+var wordID = document.getElementById('generated-word')
+for(var i = 0; i < randomWord.length; i++){
+    var newElement = document.createElement('div');
+    newElement.classList.add('word-letters');
+    newElement.setAttribute('id', 'letter' + i);
+    newElement.textContent = randomWord[i];
+    log(randomWord[i]);
+    wordID.appendChild(newElement);
+}
+
+// var wordID = document.getElementById('generated-word')
+// for(var i = 0; i < test.length; i++){
+//     var newElement = document.createElement('div');
+//     newElement.classList.add('word-letters');
+//     newElement.setAttribute('id', 'letter' + i);
+//     newElement.textContent = test[i];
+//     log(test[i]);
+//     wordID.appendChild(newElement);
+// }
+
+// Write Attempts available
+var attempts = 6;
+var attemptsID = document.getElementById('attempts');
+attemptsID.innerHTML = attempts
+
+// Set dashes array equal to word length
+var dashes = [];
+for(var i = 0; i < generateRandomWord.length; i++){
+
+    dashes.push('_ ');
     
 };
-// alert(generateRandomWord());
 
-//write word on screen
-var writeWord = function(){
-    document.getElementById('generated-word').innerHTML = generateRandomWord();
+// Write dashes
+function printDashes(){
+    for(var i = 0; i < generateRandomWord.length; i++){
+        var dashesID = document.getElementById('dashes');
+        var insertDashes = document.createTextNode(dashes[i]);
+        dashesID.appendChild(insertDashes);
+        // alert('print')
+    }
 };
-writeWord();
+printDashes();
 
+// function setAttempts(){
+
+//     if(randomWord.length <= 7){
+
+//         attemptsID.innerHTML = 3;
+
+//     } else {
+
+//         attemptsID.innerHTML = 6;
+
+//     }
+    
+// };
+// setAttempts();
+
+// alert(dashes);
 
 //hide word from view (set text color to trasparent)
-var hideWord = document.getElementById('generated-word').style.opacity = '1'; //0
+// var hideWord = document.getElementById('generated-word').style.opacity = '0'; //0
 
 
 //start game
 
 //register keystrokes
 
+
 document.onkeyup = function(event) {
-    var letterPressed = event.key.toLocaleLowerCase();
-    alert(letterPressed);
+    var letterPressed = event.key.toLowerCase();
+    // alert(letterPressed);
+
+    //compare keystrokes to word on screen
+    for(var i = 0; i < generateRandomWord.length; i++){
+        if(generateRandomWord[i] === letterPressed){
+            
+            document.getElementById('letter' + i).style.opacity = '0';
+            alert('yay');
+            var correct = true;
+            
+        }   
+    }
 };
 
-//compare keystrokes to word on screen
-/* document.onkeyup = function(event) {
-    var letterPressed = event.key.toLocaleLowerCase();
-    var i = 0;
-    var interval = setInterval(function(){
-        
-    })
-    if(letterPressed ==)
-} */
+// Attempts left -1
+// Letter used
 
 //if right, display letter
 
