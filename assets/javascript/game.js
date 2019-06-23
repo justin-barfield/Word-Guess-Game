ebe1af87db5bccaad30e775b53150c49e41236df
barfiejus <justin.kyle.barfield@gmail.com>
2019-06-23 17:10:43 -0400

    JS done, but need to add a timeout to allow the word to be completely shown before loading a new word. Need to style the page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,254 +7,161 @@ var words = [
     ['e','v','e','r','y','t','h','i','n','g'],
 ];
 
-// var test = [
-//     ["help", 'test'], 
-//     ['test'],
-// ];
+// Write Attempts available
+var attempts;
+var attemptsID = document.getElementById('attempts');
 
-//generate a random word from array
+function writeAttempts(){
+    attempts = 6;
+    attemptsID.innerHTML = attempts
+};
+writeAttempts();
 
-// log(generateRandomNumber());
 
-// log(generateRandomWord.length);
+// Write Wins
+var wins = 0;
+var scoreID = document.getElementById('games-won');
+
+function writeWins(){
+    scoreID.innerHTML = wins;
+};
+writeWins();
 
 
-// var randomWord = generateRandomWord.join(' ');
-// log(randomWord);
+// Write Losses
+var losses = 0
+var gamesLostID = document.getElementById('games-lost');
 
-// var testJoin = test.join(' ');
+function writeLosses(){
+    gamesLostID.innerHTML = losses;
+};
+writeLosses();
 
-// write word and dashes on screen
-// var writeWord = function(){
-    //     document.getElementById('generated-word').innerHTML = randomWord;
-    // };
-    
-    // Write Attempts available
-    var attempts;
-    var attemptsID = document.getElementById('attempts');
-    
-    function writeAttempts(){
-        attempts = 6;
-        attemptsID.innerHTML = attempts
+
+// Write Word;
+var randomNumber = function(){
+    return Math.floor(Math.random() * words.length);
+};
+
+var generateRandomWord = words[randomNumber()];
+log(generateRandomWord);
+var wordID = document.getElementById('generated-word');
+
+function writeWord(){
+    generateRandomWord = words[randomNumber()];
+    for(var i = 0; i < generateRandomWord.length; i++){
+        var newElement = document.createElement('div');
+        newElement.classList.add('word-letters');
+        newElement.setAttribute('id', 'letter' + i);
+        newElement.textContent = generateRandomWord[i];
+        // log(randomNumber());
+        log(generateRandomWord);
+        wordID.appendChild(newElement);
     };
-    writeAttempts();
-    
-    
-    // Write Wins
-    var wins = 0;
-    var scoreID = document.getElementById('games-won');
-    
-    function writeWins(){
-        scoreID.innerHTML = wins;
-    };
-    writeWins();
-    
-    
-    // Write Losses
-    var losses = 0
-    var gamesLostID = document.getElementById('games-lost');
-    
-    function writeLosses(){
-        gamesLostID.innerHTML = losses;
-    };
-    writeLosses();
-    
-    
-    // Write Word;
-    var randomNumber = function(){
-        return Math.floor(Math.random() * words.length);
-    };
-    log(generateRandomWord);
-    var generateRandomWord = words[randomNumber()];
-    log(generateRandomWord);
-    var wordID = document.getElementById('generated-word');
-    
-    function writeWord(){
-        randomNumber();
-        for(var i = 0; i < generateRandomWord.length; i++){
-            var newElement = document.createElement('div');
-            newElement.classList.add('word-letters');
-            newElement.setAttribute('id', 'letter' + i);
-            newElement.textContent = generateRandomWord[i];
-            // log(randomNumber());
-            log(generateRandomWord);
-            wordID.appendChild(newElement);
-        };
-    };
-    writeWord();
+};
+writeWord();
 
 // Print Dashes on page
 var dashesID = document.getElementById('dashes');
 
 function printDashes(){
-// Set dashes array equal to word length
+    // Set dashes array equal to word length
     var dashes = [];
     for(var i = 0; i < generateRandomWord.length; i++){
         dashes.push('_ ');
     };
 
-// Write dashes
+    // Write dashes
     for(var i = 0; i < generateRandomWord.length; i++){
         var insertDashes = document.createElement('div');
         insertDashes.classList.add('dashes-spacing');
         insertDashes.textContent = dashes[i];
         dashesID.appendChild(insertDashes);
-        // alert('print')
     }
 };
 printDashes();
 
 
-// function printDashes(){
-//     for(var i = 0; i < generateRandomWord.length; i++){
-//         var dashesID = document.getElementById('dashes');
-//         var insertDashes = document.createTextNode(dashes[i]);
-//         dashesID.classList.add('dashes-spacing');
-//         dashesID.appendChild(insertDashes);
-//         // alert('print')
-//     }
-// };
-
-// function setAttempts(){
-
-//     if(randomWord.length <= 7){
-
-//         attemptsID.innerHTML = 3;
-
-//     } else {
-
-//         attemptsID.innerHTML = 6;
-
-//     }
-    
-// };
-// setAttempts();
-
-// alert(dashes);
-
-//hide word from view (set text color to trasparent)
-// var hideWord = document.getElementById('generated-word').style.opacity = '0'; //0
-
-
-//start game
-
-//register keystrokes
-
-// var dashesID = document.getElementById('dashes');
-//     for(var i = 0; i < generateRandomWord.length; i++){
-//         var insertDashes = document.createElement('div');
-//         insertDashes.classList.add('dashes-spacing');
-//         insertDashes.textContent = dashes[i];
-//         dashesID.appendChild(insertDashes);
-
-var letterCount = 1;
+var letterCount = 0;
 var usedLettersArray = [];
 
 document.onkeyup = function(event) {
     var letterPressed = event.key.toLowerCase();
+    
     if(letterPressed === 'f5'){
         return;
-    }
-    for(var i = 0; i < usedLettersArray.length; i++){
-        if(usedLettersArray[i] === letterPressed){
-            alert('Recurring letter')
-            return;
-        }
-    };
-
-    usedLettersArray.push(letterPressed);
-
-    function resetLettersUsed(){
-        usedLettersArray = [];
-    };
-    
-    // log(usedLettersArray);
-    // alert(letterPressed);
-    
-    // Write letter on screen
-    var lettersUsedID = document.getElementById('used-letters');
-    // var appendLetters = lettersUsedID.createElement('div');
-    var appendLetters = document.createElement('span');
-    appendLetters.classList.add('letters-used');
-    appendLetters.textContent = letterPressed + " ";
-    lettersUsedID.appendChild(appendLetters);
-
-    var correct = false;
-
-    // Win update
-    if(letterCount === generateRandomWord.length){
-        wins++;
-        writeWins();
-        resetLettersUsed();
-        lettersUsedID.innerHTML = '';
-        wordID.innerHTML = '';
-        dashesID.innerHTML = '';
-        writeWord();
-        printDashes();
-        writeAttempts();
-        letterCount = 1;
+}
+for(var i = 0; i < usedLettersArray.length; i++){
+    if(usedLettersArray[i] === letterPressed){
+        alert('Recurring letter')
         return;
-    };
-
-    // Compare keystrokes to word on screen
-    for(var i = 0; i < generateRandomWord.length; i++){
-        
-        if(generateRandomWord[i] === letterPressed){
-            
-            document.getElementById('letter' + i).style.opacity = '0';
-            letterCount++;
-            correct = true;
-
-        };
-    };
-
-    // Attempts count
-    if(correct !== true) {
-        attempts--;
-        attemptsID.innerHTML = attempts;
     }
-
-    // Loss reset
-    if(attempts === 0){
-        losses++;
-        writeLosses();
-        resetLettersUsed();
-        lettersUsedID.innerHTML = '';
-        wordID.innerHTML = '';
-        dashesID.innerHTML = '';
-        writeWord();
-        printDashes();
-        writeAttempts();
-        letterCount = 1;
-    };
-    
-
-    // while(letterCount === generateRandomWord.length){
-    //     wins++;
-    //     writeWins();
-    //     lettersUsedID.innerHTML = '';
-    //     wordID.innerHTML = '';
-    //     dashesID.innerHTML = '';
-    //     writeWord();
-    //     printDashes();
-    //     writeAttempts();
-    //     letterCount = 0;
-    //     break;
-    // };
-
 };
 
-// Attempts left -1
-// Letter used
+usedLettersArray.push(letterPressed);
 
-//if right, display letter
+function resetLettersUsed(){
+    usedLettersArray = [];
+};
 
+// log(usedLettersArray);
+// alert(letterPressed);
 
-    //if all letters guessed, add 1 to score
+// Write letter on screen
+var lettersUsedID = document.getElementById('used-letters');
+// var appendLetters = lettersUsedID.createElement('div');
+var appendLetters = document.createElement('span');
+appendLetters.classList.add('letters-used');
+appendLetters.textContent = letterPressed + " ";
+lettersUsedID.appendChild(appendLetters);
 
+var correct = false;
 
-//if wrong, -attempts available
+// Compare keystrokes to word on screen
+for(var i = 0; i < generateRandomWord.length; i++){
+    
+    if(generateRandomWord[i] === letterPressed){
+        
+        document.getElementById('letter' + i).style.opacity = '1';
+        letterCount++;
+        correct = true;
+        // Win update
+        if(letterCount === generateRandomWord.length){
+            alert('You win!');
+            // Need to set timeout
+            wins++;
+            writeWins();
+            resetLettersUsed();
+            lettersUsedID.innerHTML = '';
+            wordID.innerHTML = '';
+            dashesID.innerHTML = '';
+            writeWord();
+            printDashes();
+            writeAttempts();
+            letterCount = 0;
+            return;
+        };
+    };
+};
 
+// Attempts count
+if(correct !== true) {
+    attempts--;
+    attemptsID.innerHTML = attempts;
+}
 
-    //if no attempts left, add 1 to losses, restart game with new word
+// Loss reset
+if(attempts === 0){
+    losses++;
+    writeLosses();
+    resetLettersUsed();
+    lettersUsedID.innerHTML = '';
+    wordID.innerHTML = '';
+    dashesID.innerHTML = '';
+    writeWord();
+    printDashes();
+    writeAttempts();
+    letterCount = 0;
+};
 
+}
